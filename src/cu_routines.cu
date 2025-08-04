8290165b1d49d4e1a4275c897ef453b4baae936b
Gaurav Harsha <gauravharsha05@gmail.com>
2025-08-04 13:06:13 -0400

    Device rank checks

--- src/cu_routines.cu
+++ src/cu_routines.cu
@@ -239,7 +239,7 @@ namespace green::gpu {
                                int verbose, irre_pos_callback& irre_pos, mom_cons_callback& momentum_conservation,
                                gw_reader1_callback<prec>& r1, gw_reader2_callback<prec>& r2) {
     // this is the main GW loop
-    PUSH_RANGE("Solve in CU_ROUTINES begin", 0);
+    if (!_devices_rank) PUSH_RANGE("Solve in CU_ROUTINES begin", 0);
     if (!_devices_rank && verbose > 0) std::cout << "GW main loop" << std::endl;
     qpt.verbose() = verbose;
 
@@ -247,7 +247,7 @@ namespace green::gpu {
       if (verbose > 2) std::cout << "q = " << q_reduced_id << std::endl;
       size_t q = reduced_to_full[q_reduced_id];
       qpt.reset_Pqk0();
-      PUSH_RANGE("Build P0", 1);
+      if (!_devices_rank) PUSH_RANGE("Build P0", 1);
       for (size_t k = 0; k < _nk; ++k) {
         std::array<size_t, 4> k_vector      = momentum_conservation({
             {k, 0, q}
@@ -258,12 +258,12 @@ namespace green::gpu {
         bool                  need_minus_k  = reduced_to_full[k_reduced_id] != k;
         bool                  need_minus_k1 = reduced_to_full[k1_reduced_id] != k1;
 
-        PUSH_RANGE("r1: read ints and G(k2)", 2);
+        if (!_devices_rank) PUSH_RANGE("r1: read ints and G(k2)", 2);
         r1(k, k1, k_reduced_id, k1_reduced_id, k_vector, V_Qpm, Vk1k2_Qij, Gk_smtij, Gk1_stij, need_minus_k, need_minus_k1);
-        POP_RANGE;
+        if (!_devices_rank) POP_RANGE;
 
         gw_qkpt<prec>* qkpt = obtain_idle_qkpt(qkpts);
-        PUSH_RANGE("setup: copy data to device", 3);
+        if (!_devices_rank) PUSH_RANGE("setup: copy data to device", 3);
         if (_low_device_memory) {
           if (!_X2C) {
             qkpt->set_up_qkpt_first(Gk1_stij.data(), Gk_smtij.data(), V_Qpm.data(), k_reduced_id, need_minus_k, k1_reduced_id,
@@ -275,20 +275,20 @@ namespace green::gpu {
         } else {
           qkpt->set_up_qkpt_first(nullptr, nullptr, V_Qpm.data(), k_reduced_id, need_minus_k, k1_reduced_id, need_minus_k1);
         }
-        POP_RANGE;
-        PUSH_RANGE("P0 contraction", 4);
+        if (!_devices_rank) POP_RANGE;
+        if (!_devices_rank) PUSH_RANGE("P0 contraction", 4);
         qkpt->compute_first_tau_contraction(qpt.Pqk0_tQP(qkpt->all_done_event()), qpt.Pqk0_tQP_lock());
-        POP_RANGE;
+        if (!_devices_rank) POP_RANGE;
       }
       qpt.wait_for_kpts();
-      POP_RANGE;
-      PUSH_RANGE("Build P", 1);
+      if (!_devices_rank) POP_RANGE;
+      if (!_devices_rank) PUSH_RANGE("Build P", 1);
       qpt.scale_Pq0_tQP(1. / _nk);
       qpt.transform_tw();
       qpt.compute_Pq();
       qpt.transform_wt();
-      POP_RANGE;
-      PUSH_RANGE("Build Sigma", 1);
+      if (!_devices_rank) POP_RANGE;
+      if (!_devices_rank) PUSH_RANGE("Build Sigma", 1);
 
       // Write to Sigma(k), k belongs to _ink
       for (size_t k_reduced_id = 0; k_reduced_id < _ink; ++k_reduced_id) {
@@ -303,35 +303,35 @@ namespace green::gpu {
             bool                  need_minus_k1 = reduced_to_full[k1_reduced_id] != k1;
             bool                  need_minus_q  = reduced_to_full[q_reduced_id] != q_or_qinv;
 
-            PUSH_RANGE("r2: read ints and G(k3)", 2);
+            if (!_devices_rank) PUSH_RANGE("r2: read ints and G(k3)", 2);
             r2(k, k1, k1_reduced_id, k_vector, V_Qim, Vk1k2_Qij, Gk1_stij, need_minus_k1);
-            POP_RANGE;
+            if (!_devices_rank) POP_RANGE;
 
             gw_qkpt<prec>* qkpt = obtain_idle_qkpt(qkpts);
             if (_low_device_memory) {
               if (!_X2C) {
-                PUSH_RANGE("setup: copy data to device", 3);
+                if (!_devices_rank) PUSH_RANGE("setup: copy data to device", 3);
                 qkpt->set_up_qkpt_second(Gk1_stij.data(), V_Qim.data(), k_reduced_id, k1_reduced_id, need_minus_k1);
-                POP_RANGE;
-                PUSH_RANGE("Sigma contraction", 4);
+                if (!_devices_rank) POP_RANGE;
+                if (!_devices_rank) PUSH_RANGE("Sigma contraction", 4);
                 qkpt->compute_second_tau_contraction(Sigmak_stij.data(),
                                                      qpt.Pqk_tQP(qkpt->all_done_event(), qkpt->stream(), need_minus_q));
-                POP_RANGE;
-                PUSH_RANGE("copy sigma back to host", 3);
+                if (!_devices_rank) POP_RANGE;
+                if (!_devices_rank) PUSH_RANGE("copy sigma back to host", 3);
                 copy_Sigma(Sigma_tskij_host, Sigmak_stij, k_reduced_id, _nts, _ns);
-                POP_RANGE;
+                if (!_devices_rank) POP_RANGE;
               } else {
-                PUSH_RANGE("setup: copy data to device", 3);
+                if (!_devices_rank) PUSH_RANGE("setup: copy data to device", 3);
                 // In 2cGW, G(-k) = G*(k) has already been addressed in r2()
                 qkpt->set_up_qkpt_second(Gk1_stij.data(), V_Qim.data(), k_reduced_id, k1_reduced_id, false);
-                POP_RANGE;
-                PUSH_RANGE("Sigma contraction", 4);
+                if (!_devices_rank) POP_RANGE;
+                if (!_devices_rank) PUSH_RANGE("Sigma contraction", 4);
                 qkpt->compute_second_tau_contraction_2C(Sigmak_stij.data(),
                                                         qpt.Pqk_tQP(qkpt->all_done_event(), qkpt->stream(), need_minus_q));
-                POP_RANGE;
-                PUSH_RANGE("copy sigma back to host", 3);
+                if (!_devices_rank) POP_RANGE;
+                if (!_devices_rank) PUSH_RANGE("copy sigma back to host", 3);
                 copy_Sigma_2c(Sigma_tskij_host, Sigmak_stij, k_reduced_id, _nts);
-                POP_RANGE;
+                if (!_devices_rank) POP_RANGE;
               }
             } else {
               qkpt->set_up_qkpt_second(nullptr, V_Qim.data(), k_reduced_id, k1_reduced_id, need_minus_k1);
@@ -340,13 +340,13 @@ namespace green::gpu {
           }
         }
       }
-      POP_RANGE;
+      if (!_devices_rank) POP_RANGE;
     }
     cudaDeviceSynchronize();
     if (!_low_device_memory and !_X2C) {
       copy_Sigma_from_device_to_host(sigma_kstij_device, Sigma_tskij_host.data(), _ink, _nao, _nts, _ns);
     }
-    POP_RANGE;
+    if (!_devices_rank) POP_RANGE;
   }
 
   template <typename prec>
